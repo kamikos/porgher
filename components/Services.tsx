df1cd2c16b79aa46a37c0b89532c54405531b6a0
John Doe <johndoe@example.com>
2025-06-24 10:17:35 +0200

    Services layout and correct icons

--- components/Services.tsx
+++ components/Services.tsx
@@ -1,132 +1,113 @@
 export default function Services() {
   const PortraitIcon = (
     <svg
-      className="size-12"
-      viewBox="0 0 24 24"
+      width="66"
+      height="66"
+      viewBox="0 0 66 66"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <rect
-        x="3"
-        y="5"
-        width="18"
-        height="14"
-        rx="2"
-        stroke="currentColor"
-        strokeWidth="1.5"
-      />
-      <path
-        d="M10 15V9L15 12L10 15Z"
-        stroke="currentColor"
-        strokeWidth="1.5"
-        strokeLinejoin="round"
-        strokeLinecap="round"
-      />
+      <mask
+        id="mask0_9_41"
+        style={{ maskType: "alpha" }}
+        maskUnits="userSpaceOnUse"
+        x="0"
+        y="0"
+        width="66"
+        height="66"
+      >
+        <rect width="65.4092" height="65.4092" fill="#D9D9D9" />
+      </mask>
+      <g mask="url(#mask0_9_41)">
+        <path
+          d="M33.3859 35.7707L48.0873 25.8912L33.3859 16.0116V35.7707ZM40.273 51.0485H49.9269C49.7661 51.9674 49.3828 52.7248 48.7768 53.3208C48.1704 53.9163 47.3937 54.2874 46.4466 54.4341L16.1901 58.176C14.8306 58.3686 13.6103 58.0473 12.5292 57.2119C11.4477 56.3771 10.8247 55.2799 10.6603 53.9203L7.18068 25.7971C7.01625 24.4376 7.34897 23.2155 8.17885 22.1308C9.00873 21.0457 10.1032 20.4297 11.4623 20.283L14.3607 19.9791V24.0672L12.0128 24.3295C11.768 24.3468 11.5758 24.4472 11.4364 24.6307C11.2965 24.8142 11.244 25.0281 11.279 25.2725L14.7484 53.3964C14.7834 53.6412 14.8883 53.8334 15.0632 53.9728C15.2376 54.1127 15.4472 54.1652 15.6921 54.1302L40.273 51.0485ZM24.7383 45.5977C23.3615 45.5977 22.1962 45.1208 21.2423 44.1669C20.2884 43.213 19.8115 42.0477 19.8115 40.6709V11.7403C19.8115 10.3635 20.2884 9.19819 21.2423 8.2443C22.1962 7.29042 23.3615 6.81348 24.7383 6.81348H53.6689C55.0457 6.81348 56.211 7.29042 57.1649 8.2443C58.1188 9.19819 58.5957 10.3635 58.5957 11.7403V40.6709C58.5957 42.0477 58.1188 43.213 57.1649 44.1669C56.211 45.1208 55.0457 45.5977 53.6689 45.5977H24.7383ZM24.7383 41.5096H53.6689C53.9137 41.5096 54.1147 41.4311 54.2719 41.2739C54.429 41.1167 54.5076 40.9157 54.5076 40.6709V11.7403C54.5076 11.4955 54.429 11.2945 54.2719 11.1373C54.1147 10.9801 53.9137 10.9016 53.6689 10.9016H24.7383C24.4934 10.9016 24.2925 10.9801 24.1353 11.1373C23.9781 11.2945 23.8995 11.4955 23.8995 11.7403V40.6709C23.8995 40.9157 23.9781 41.1167 24.1353 41.2739C24.2925 41.4311 24.4934 41.5096 24.7383 41.5096Z"
+          fill="#FCFCF6"
+        />
+      </g>
     </svg>
   );
 
   const EventIcon = (
     <svg
-      className="size-12"
-      viewBox="0 0 24 24"
+      width="66"
+      height="66"
+      viewBox="0 0 66 66"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <path
-        d="M2 8C2 5.17157 2 3.75736 2.87868 2.87868C3.75736 2 5.17157 2 8 2H16C18.8284 2 20.2426 2 21.1213 2.87868C22 3.75736 22 5.17157 22 8V16C22 18.8284 22 20.2426 21.1213 21.1213C20.2426 22 18.8284 22 16 22H8C5.17157 22 3.75736 22 2.87868 21.1213C2 20.2426 2 18.8284 2 16V8Z"
-        stroke="currentColor"
-        strokeWidth="1.5"
-      />
-      <circle cx="12" cy="13" r="4" stroke="currentColor" strokeWidth="1.5" />
-      <circle cx="18.5" cy="6.5" r="1" fill="currentColor" />
+      <mask
+        id="mask0_9_77"
+        style={{ maskType: "alpha" }}
+        maskUnits="userSpaceOnUse"
+        x="0"
+        y="0"
+        width="66"
+        height="66"
+      >
+        <rect width="65.4092" height="65.4092" fill="#D9D9D9" />
+      </mask>
+      <g mask="url(#mask0_9_77)">
+        <path
+          d="M32.7046 20.4404C30.0814 20.4404 27.8359 21.3745 25.9681 23.2428C24.0999 25.1106 23.1658 27.356 23.1658 29.9792C23.1658 32.6024 24.0999 34.8479 25.9681 36.7157C27.8359 38.5839 30.0814 39.5181 32.7046 39.5181C35.3278 39.5181 37.5733 38.5839 39.4411 36.7157C41.3093 34.8479 42.2434 32.6024 42.2434 29.9792C42.2434 27.356 41.3093 25.1106 39.4411 23.2428C37.5733 21.3745 35.3278 20.4404 32.7046 20.4404ZM32.7046 35.43C31.2056 35.43 29.9224 34.8963 28.855 33.8288C27.7875 32.7614 27.2538 31.4782 27.2538 29.9792C27.2538 28.4803 27.7875 27.1971 28.855 26.1296C29.9224 25.0622 31.2056 24.5285 32.7046 24.5285C34.2036 24.5285 35.4868 25.0622 36.5542 26.1296C37.6216 27.1971 38.1554 28.4803 38.1554 29.9792C38.1554 31.4782 37.6216 32.7614 36.5542 33.8288C35.4868 34.8963 34.2036 35.43 32.7046 35.43ZM43.6082 46.0167C44.2918 46.0167 44.8641 45.7855 45.3252 45.3231C45.7862 44.8607 46.0167 44.2877 46.0167 43.6041C46.0167 42.9205 45.7855 42.3482 45.3231 41.8871C44.8607 41.4261 44.2877 41.1955 43.6041 41.1955C42.9205 41.1955 42.3481 41.4267 41.8871 41.8891C41.426 42.3516 41.1955 42.9246 41.1955 43.6082C41.1955 44.2918 41.4267 44.8641 41.8891 45.3252C42.3515 45.7862 42.9245 46.0167 43.6082 46.0167ZM12.2642 57.5472V19.0777C12.2642 15.6746 13.4575 12.7798 15.844 10.3933C18.2305 8.00674 21.1253 6.81348 24.5284 6.81348H40.8807C44.2838 6.81348 47.1786 8.00674 49.5652 10.3933C51.9517 12.7798 53.145 15.6746 53.145 19.0777V57.5472H12.2642ZM16.3523 49.0569H49.0569V19.0777C49.0569 16.8065 48.262 14.8761 46.6722 13.2863C45.0824 11.6965 43.1519 10.9016 40.8807 10.9016H24.5284C22.2573 10.9016 20.3268 11.6965 18.737 13.2863C17.1472 14.8761 16.3523 16.8065 16.3523 19.0777V49.0569Z"
+          fill="#FCFCF6"
+        />
+      </g>
     </svg>
   );
 
   const ProductIcon = (
     <svg
-      className="size-12"
-      viewBox="0 0 24 24"
+      width="66"
+      height="66"
+      viewBox="0 0 66 66"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <path
-        d="M5 19V8.2C5 7.0799 5.89543 6.2 7 6.2H17C18.1046 6.2 19 7.0799 19 8.2V19"
-        stroke="currentColor"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M9 6.2V5C9 3.34315 10.3431 2 12 2C13.6569 2 15 3.34315 15 5V6.2"
-        stroke="currentColor"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M5 19H19"
-        stroke="currentColor"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M15 11L12 14L10 12"
-        stroke="currentColor"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
+      <mask
+        id="mask0_9_83"
+        style={{ maskType: "alpha" }}
+        maskUnits="userSpaceOnUse"
+        x="0"
+        y="0"
+        width="66"
+        height="66"
+      >
+        <rect width="65.4092" height="65.4092" fill="#D9D9D9" />
+      </mask>
+      <g mask="url(#mask0_9_83)">
+        <path
+          d="M35.2256 30.1155L24.6123 19.5022L27.5264 16.6562L35.2256 24.3554L50.6921 8.95703L53.5381 11.803L35.2256 30.1155ZM18.2655 50.6295L38.045 56.2267L54.3135 51.1848C54.174 50.5666 53.884 50.0924 53.4434 49.7621C53.0032 49.4319 52.5106 49.2668 51.9656 49.2668H38.6221C37.4306 49.2668 36.4191 49.2214 35.5874 49.1305C34.7557 49.0397 33.9013 48.8405 33.0241 48.533L26.8716 46.4992L28.0817 42.4949L33.6006 44.4136C34.4254 44.6929 35.3777 44.8833 36.4574 44.9845C37.5372 45.0858 39.0727 45.1506 41.064 45.1787C41.064 44.5042 40.9121 43.9223 40.6082 43.4331C40.3043 42.9439 39.9041 42.6137 39.4077 42.4424L23.5957 36.6353C23.543 36.6181 23.4948 36.6051 23.4512 36.5965C23.4076 36.5874 23.3597 36.5829 23.3075 36.5829H18.2655V50.6295ZM4.6386 58.5958V32.4955H23.2809C23.5639 32.4955 23.8514 32.525 24.1435 32.5841C24.4351 32.6436 24.7083 32.7187 24.9631 32.8096L40.8439 38.6589C42.0808 39.1163 43.1081 39.926 43.9257 41.0879C44.7433 42.2499 45.1521 43.6134 45.1521 45.1787H51.9656C53.9224 45.1787 55.5165 45.8103 56.7479 47.0736C57.9798 48.3363 58.5957 49.9759 58.5957 51.9922V54.1929L38.1812 60.5349L18.2655 54.8538V58.5958H4.6386ZM8.72668 54.5077H14.1774V36.5829H8.72668V54.5077Z"
+          fill="#FCFCF6"
+        />
+      </g>
     </svg>
   );
 
   const LifestyleIcon = (
     <svg
-      className="size-12"
-      viewBox="0 0 24 24"
+      width="66"
+      height="66"
+      viewBox="0 0 66 66"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <path
-        d="M17.5 10.4C17.5 12.1673 16.0673 13.6 14.3 13.6C12.5327 13.6 11.1 12.1673 11.1 10.4C11.1 8.63269 12.5327 7.2 14.3 7.2C16.0673 7.2 17.5 8.63269 17.5 10.4Z"
-        stroke="currentColor"
-        strokeWidth="1.5"
-      />
-      <path
-        d="M13.3008 11.3999L14.3008 9.3999L15.3008 11.3999"
-        stroke="currentColor"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M13.7012 10.7H14.9012"
-        stroke="currentColor"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M6.5 18V15"
-        stroke="currentColor"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M8.5 18V14"
-        stroke="currentColor"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M10.5 18V16"
-        stroke="currentColor"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
+      <mask
+        id="mask0_9_89"
+        style={{ maskType: "alpha" }}
+        maskUnits="userSpaceOnUse"
+        x="0"
+        y="0"
+        width="66"
+        height="66"
+      >
+        <rect width="65.4092" height="65.4092" fill="#D9D9D9" />
+      </mask>
+      <g mask="url(#mask0_9_89)">
+        <path
+          d="M29.8429 25.2309L32.5683 17.5371H32.7884L35.5662 25.2309H29.8429ZM32.7107 39.518C28.5486 39.518 25.0088 38.0613 22.0913 35.1479C19.1737 32.2344 17.715 28.6967 17.715 24.5346C17.715 20.3724 19.1717 16.8326 22.0851 13.9151C24.9986 10.9976 28.5363 9.53882 32.6985 9.53882C36.8606 9.53882 40.4004 10.9955 43.3179 13.909C46.2354 16.8224 47.6942 20.3602 47.6942 24.5223C47.6942 28.6844 46.2375 32.2242 43.324 35.1417C40.4106 38.0593 36.8728 39.518 32.7107 39.518ZM24.476 32.3373H27.2225L28.994 27.589H36.4152L38.1867 32.3373H41.0381L33.9208 13.9941H31.3521L24.476 32.3373ZM9.32898 59.2771V47.0128H13.4171V59.2771H9.32898ZM19.3925 59.2771V47.0128H23.4798V59.2771H19.3925ZM29.4552 59.2771V47.0128H33.5433V59.2771H29.4552ZM39.518 59.2771V47.0128H56.0802V59.2771H39.518Z"
+          fill="#FCFCF6"
+        />
+      </g>
     </svg>
   );
 
@@ -164,41 +145,59 @@ export default function Services() {
   return (
     <section
       id="services"
-      className="bg-[#1a1919] text-[#fcfcf6] py-20 px-4 sm:px-6 lg:px-8"
+      className="bg-[#1a1919] text-[#fcfcf6] py-20 px-4 sm:px-6 lg:px-8 font-albert-sans"
     >
       <div className="max-w-7xl mx-auto">
         <div className="flex flex-col md:flex-row justify-between items-start mb-16 gap-8">
-          <h2 className="w-full max-w-2xl text-5xl md:text-6xl font-normal font-['Albert_Sans'] uppercase">
+          <h2 className="w-full max-w-2xl text-5xl md:text-6xl font-normal uppercase">
             Creative Photography, Tailored for You.
           </h2>
-          <div className="hidden md:flex flex-col items-center gap-4 flex-shrink-0">
-            <button className="relative group size-32 rounded-full border-2 border-[#fcfcf6] flex items-center justify-center text-xl font-medium font-['Albert_Sans'] uppercase">
-              <div className="absolute size-full rounded-full scale-0 group-hover:scale-100 transition-transform duration-300 bg-[#fcfcf6]"></div>
-              <span className="relative group-hover:text-[#1a1919] transition-colors duration-300">
-                Explore now
+            <div className="hidden md:flex flex-col items-center gap-4 flex-shrink-0">
+            <button className="group flex items-center hover:scale-105 transition-transform duration-300">
+              <div className="relative size-24 flex-shrink-0">
+              <svg
+                className="transition-transform duration-500 ease-in-out group-hover:rotate-180 group-hover:scale-110"
+                width="94"
+                height="94"
+                viewBox="0 0 94 94"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle
+                cx="47"
+                cy="47"
+                r="45.5"
+                stroke="#FCFCF6"
+                strokeWidth="3"
+                className="transition-colors duration-300 group-hover:stroke-[#C4C4C4]"
+                />
+              </svg>
+              </div>
+              <span className="relative -ml-8 bg-[#1a1919] py-1 pl-2 pr-4 text-xl font-medium uppercase transition-colors duration-300 group-hover:text-[#C4C4C4]">
+              Explore now
               </span>
             </button>
-          </div>
+            </div>
         </div>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 grid-rows-5">
           {services.map((service, index) => (
             <div
               key={index}
-              className={`p-8 flex flex-col justify-between min-h-[480px] transition-colors duration-300 row-span-4 grid-col-${index + 1} ${
-                service.staggered ? "row-start-2" : `row-start-1`
-              } ${
+              className={`p-8 flex flex-col justify-between min-h-[480px] transition-colors duration-300 row-span-4 grid-col-${
+                index + 1
+              } ${service.staggered ? "row-start-2" : `row-start-1`} ${
                 service.highlighted
-                  ? "bg-stone-200 text-stone-900"
+                  ? "bg-[#C4C4C4] text-stone-900"
                   : "border border-[#fcfcf6]"
               }`}
             >
-              <div>
                 <div className="mb-8">{service.icon}</div>
-                <h3 className="text-3xl font-medium font-['Albert_Sans'] uppercase mb-4">
+              <div className="mt-auto">
+                <h3 className="text-3xl font-medium uppercase mb-4 text-[#FCFCF6]">
                   {service.title}
                 </h3>
-                <p className="opacity-70 text-xl font-normal font-['Albert_Sans'] mb-4">
+                <p className="opacity-70 text-lg font-normal mb-4 text-[#FCFCF6]">
                   {service.description}
                 </p>
               </div>
@@ -207,9 +206,7 @@ export default function Services() {
                 className="inline-flex justify-start items-center gap-4 group"
               >
                 <span
-                  className={`text-xl font-medium font-['Albert_Sans'] uppercase ${
-                    service.highlighted ? "text-stone-900" : "text-[#fcfcf6]"
-                  }`}
+                  className={`text-xl font-medium uppercase text-[#FCFCF6]`}
                 >
                   Read More
                 </span>
